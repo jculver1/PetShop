f4086610834263e7c2e95ef816c969c9b567e92f
Jessica culver <jculver@Jessicas-Air.lan>
2022-04-24 15:28:07 -0400

    setup express server and get and put endpoints

--- server/index.js
+++ server/index.js
@@ -1,35 +1,45 @@
-const http = require('http');
-/**
- * Very simple endpoint, feel free to use a library to make your life easier, Rest or Graph, both work
- */
-const server = http.createServer((req, res) => {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Request-Method', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+let catJson = require('./data/catData.json');
+let dogJson = require('./data/dogData.json');
+const express = require('express')
+const server = express()
+var cors = require('cors')
+const parser = require('body-parser')
 
-  if (req.url === '/') {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({
-      cats: 'http://localhost:3000/cats'
-    }))
-  }
-  if (req.url === '/cats') {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify([{
-      type: 'cat',
-      name: 'Vasia',
-      status: 'Adopted',
-      someOther: 'Some Other',
-    },{
-      type: 'cat',
-      name: 'Musci',
-      status: 'Adopted',
-      someOther: 'New Value',
+server.use(express.json())
+server.use(cors())
+server.use(parser.json())
+
+const editPet = (id, jsonObj, newInfo) => {
+  for (var i = 0; i < jsonObj.length; i++) {
+    if (jsonObj[i].name === id) {
+      console.log(jsonObj, jsonObj[i], newInfo, 'test')
+      jsonObj[i] = newInfo;
+      return;
     }
-    ]))
   }
+}
+
+server.get('/', (req, res) => {
+  let allAnimals = [...catJson, ...dogJason]
+  res.send(JSON.stringify(allAnimals))
+})
+
+server.get('/cats', (req, res) => {
+  res.send(JSON.stringify(catJson))
+})
+
+server.put('/cat/:id', (req, res) => {
+  editPet(req.params.id, catJson, req.body)
+  res.send(JSON.stringify(catJson))
+})
+
+server.get('/dogs', (req, res) => {
+  res.send(JSON.stringify(dogJson))
+})
+
+server.put('/dog/:id', (req, res) => {
+  editPet(req.params.id, dogJson, req.body)
+  res.send(JSON.stringify(dogJson))
 })
 
 server.listen(3000, (err) => {
